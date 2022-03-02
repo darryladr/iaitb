42293d1a5640d72fbe4ba39a92fb74528765a546
darryladr <darryl.adriansyah@binus.ac.id>
2022-03-02 15:53:40 +0700

    Change metadata

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 import './App.scss';
 
 import AppRoutes from './config/Routes';
@@ -6,15 +7,15 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 
 function App() {
-	return (
-		<Router basename='/iaitb'>
-			<>
-				<Header />
-				<AppRoutes />
-				<Footer />
-			</>
-		</Router>
-	);
+    return (
+        <Router basename='/iaitb'>
+            <>
+                <Header />
+                <AppRoutes />
+                <Footer />
+            </>
+        </Router>
+    );
 }
 
 export default App;
